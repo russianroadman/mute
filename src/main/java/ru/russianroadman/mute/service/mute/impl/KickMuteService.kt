a954d05306941dd3ecd68fc59fe14566f9f43790
russianroadman <realgar2@gmail.com>
2022-07-28 20:47:35 +0800

    Added kick from chat service

---
+++ src/main/java/ru/russianroadman/mute/service/mute/impl/KickMuteService.kt
@@ -0,0 +1,58 @@
+package ru.russianroadman.mute.service.mute.impl
+
+import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.groupadministration.BanChatMember
+import org.telegram.telegrambots.meta.api.objects.Message
+import org.telegram.telegrambots.meta.api.objects.User
+import ru.russianroadman.mute.Bot
+import ru.russianroadman.mute.service.mute.BanService
+import java.time.Duration
+
+@Service
+class KickMuteService(
+    private val bot: Bot
+) : BanService {
+
+    private var kickDurationMillis = 5 * 60 * 1000L
+
+    override fun examine(message: Message): Boolean {
+        if (message.hasVoice()){
+            kick(message.chatId.toString(), message.from.id)
+            return true
+        }
+        return false
+    }
+
+    override fun getName(): String {
+        return "Kick"
+    }
+
+    override fun ban(userId: Long, chatId: String) {
+        return
+    }
+
+    override fun ban(user: User, chatId: String) {
+        ban(user.id, chatId)
+    }
+
+    override fun unban(user: User, chatId: String) {
+        unban(user.userName, chatId)
+    }
+
+    override fun unban(userLogin: String, chatId: String) {
+        return
+    }
+
+    override fun setTimeoutDuration(millis: Long) {
+        kickDurationMillis = millis
+    }
+
+    private fun kick(chatId: String, userId: Long) {
+        val banHammer = BanChatMember()
+        banHammer.chatId = chatId
+        banHammer.userId = userId
+        banHammer.forTimePeriodDuration(Duration.ofMillis(kickDurationMillis))
+        bot.execute(banHammer)
+    }
+
+}
