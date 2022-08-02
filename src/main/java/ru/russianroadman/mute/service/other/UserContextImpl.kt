34f0d5cebefc1162f0897804acf47c8459ecc2cd
russianroadman <realgar2@gmail.com>
2022-08-03 00:40:45 +0800

    fix

--- src/main/java/ru/russianroadman/mute/service/other/UserContextImpl.kt
+++ src/main/java/ru/russianroadman/mute/service/other/UserContextImpl.kt
@@ -1,10 +1,13 @@
 package ru.russianroadman.mute.service.other
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.User
 
 @Service
 class UserContextImpl : UserContext {
+
+    private val log = LoggerFactory.getLogger(javaClass)
 
     /**
      * Map of chats and its users
@@ -14,7 +17,10 @@ class UserContextImpl : UserContext {
     private val userMap = mutableMapOf<String, MutableSet<User>>()
 
     override fun put(chatId: String, user: User) {
-        userMap[chatId]?.add(user)
+        userMap
+            .getOrPut(chatId) { mutableSetOf() }
+            .add(user)
+        log.info("putting user @${user.userName} to context")
     }
 
     override fun put(chatId: String, users: Iterable<User>) {
@@ -25,7 +31,7 @@ class UserContextImpl : UserContext {
 
     override fun get(username: String, chatId: String): User {
         return userMap[chatId]
-            ?.first { it.userName == username }
+            ?.first { it.userName == username.replace("@", "") }
             ?: throw NoSuchElementException(
                 "Username $username not present in context for chatId: $chatId"
             )
