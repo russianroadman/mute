37432967e37de21abcb10752dc91d8719d300388
russianroadman <realgar2@gmail.com>
2022-08-02 23:58:23 +0800

    putting user in context on message receive

--- src/main/java/ru/russianroadman/mute/service/tgapi/impl/TelegramUpdateHandler.kt
+++ src/main/java/ru/russianroadman/mute/service/tgapi/impl/TelegramUpdateHandler.kt
@@ -4,17 +4,20 @@ import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
 import ru.russianroadman.mute.service.mute.MuteSelector
+import ru.russianroadman.mute.service.other.UserContext
 import ru.russianroadman.mute.service.tgapi.CommandHandler
 import ru.russianroadman.mute.service.tgapi.UpdateHandler
 
 @Service
 class TelegramUpdateHandler(
     private val muteSelector: MuteSelector,
-    private val commandHandler: CommandHandler
+    private val commandHandler: CommandHandler,
+    private val userContext: UserContext
 ) : UpdateHandler {
 
     override fun onUpdateReceived(update: Update) {
         val message = update.message ?: return
+        userContext.put(message.chatId.toString(), message.from)
         if (!handleMessage(message) && message.isCommand) {
             handleCommand(message)
         }
