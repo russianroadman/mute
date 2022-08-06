df6bd865061dfdd82e61ae887144822f90141b8a
russianroadman <realgar2@gmail.com>
2022-08-06 13:30:09 +0800

    is localhost reachable check

--- src/main/java/ru/russianroadman/mute/service/other/PingService.kt
+++ src/main/java/ru/russianroadman/mute/service/other/PingService.kt
@@ -1,13 +1,24 @@
 package ru.russianroadman.mute.service.other
 
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.net.InetAddress
+import javax.servlet.http.HttpServletRequest
 
 @Service
-class PingService {
+class PingService(
+    private val servlet: HttpServletRequest
+) {
+
+    private val restTemplate = RestTemplate()
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Scheduled(fixedRate = 1000)
     fun pingSelf(){
+
+        InetAddress.getLocalHost().isReachable(10)
 
     }
 
